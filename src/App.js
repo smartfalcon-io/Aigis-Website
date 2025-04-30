232ccb7696b06cc0ad83434dbfe8b055a309a738
Uday Kumar Goli <goliudaykumar29@gmail.com>
2025-04-30 15:00:47 +0530

    #

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from 'react'
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { HashRouter as Router, Routes, Route } from "react-router-dom";
 import Home from './pages/home';
 import About from './pages/about';
 import Features from './pages/features';
@@ -9,12 +9,12 @@ import Header from './components/header';
 const App = () => {
   return (
     <Router>
-      <Header/>
+      <Header />
       <Routes>
-        <Route path='/' element={<Home/>} />
-        <Route path='/about' element={<About/>}/>
-        <Route path='/features' element={<Features/>}/>
-        <Route path='/contact' element={<Contact/>}/>
+        <Route path='/' element={<Home />} />
+        <Route path='/about' element={<About />} />
+        <Route path='/features' element={<Features />} />
+        <Route path='/contact' element={<Contact />} />
       </Routes>
     </Router>
   )
